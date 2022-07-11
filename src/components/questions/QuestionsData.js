af44eac5ee9ef4afcdf476b9c34ffad6c324a7e2
jokk3r <jokzer43@gmail.com>
2022-07-12 00:02:19 +0200

    add questions data

--- src/components/questions/QuestionsData.js
+++ src/components/questions/QuestionsData.js
@@ -16,7 +16,43 @@ export const QuestionsData = [
     },
     {
         id:3,
-        heading: "Do I need a powerful PC or any solver knowledge?  ",
+        heading: "Do I need a powerful PC or any solver knowledge? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:4,
+        heading: "How does the solver manage balanced actions ? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:5,
+        heading: "When will be post-flop available? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:6,
+        heading: "Can I request my own sims to be added? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:7,
+        heading: "Can I export sims? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:8,
+        heading: "Can I use it on my phone? ",
+        content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
+    },
+    {
+        id:9,
+        heading: "I have a question, how can I contact you? ",
         content:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat eleifend tristique scelerisque eget.",
     },
